6ffd36408f7f7ed42be2d4ce1ccf418264d85654
Navkiran Singh <88303778+Inavkiran@users.noreply.github.com>
2022-04-10 23:39:52 +0000

    create files

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
           .then(data => 
             {document.querySelector('.img').src = data.drinks[0].strDrinkThumb
 document.querySelector('#nameOfDrink').innerText = data.drinks[0].strDrink
-             document.querySelector('#instructions').innerText = data.drinks[0].strInstructions
+             document.querySelector('#inst').innerText = data.drinks[0].strInstructions
              console.log(data)
             }
                 )
